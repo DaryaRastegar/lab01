abcb79b79632d5f34a14b5f4ebbfd9000a8aa5fd
darya rastegar <daryarastegar1370@gmail.com>
2026-03-05 20:38:56 +1300

    first commit

---
+++ src/MyFirstProgram.java
@@ -0,0 +1,10 @@
+public class MyFirstProgram {
+    public void start() {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        MyFirstProgram p = new MyFirstProgram();
+        p.start();
+    }
+}
